6dc5fc895b40454541fd51e01bcb8bec23ad034f
Idrisabdi182 <152632180+Idrisabdi182@users.noreply.github.com>
2025-10-21 11:37:50 +0200

    Update BipRed.java

--- BipRed.java
+++ BipRed.java
@@ -10,33 +10,56 @@
 
 public class BipRed {
     Kattio io;
+    int x;
+    int y;
+    int e;
+    List<List<Integer>> edges;
+    List<List<Integer>> matchededges;
     
     void readBipartiteGraph() {
 	// Läs antal hörn och kanter
-	int x = io.getInt();
-	int y = io.getInt();
-	int e = io.getInt();
-	
+	x = io.getInt();
+	y = io.getInt();
+	e = io.getInt();
+	edges = new ArrayList<>();
 	// Läs in kanterna
 	for (int i = 0; i < e; ++i) {
+        List<Integer> currentedge = new ArrayList<>();
 	    int a = io.getInt();
 	    int b = io.getInt();
+        currentedge.add(a);
+        currentedge.add(b);
+        edges.add(currentedge);
 	}
     }
     
     
     void writeFlowGraph() {
-	int v = 23, e = 0, s = 1, t = 2;
+	int v = x + y + 2, e = e, s = 0, t = x + y + 1;
+
 	
 	// Skriv ut antal hörn och kanter samt källa och sänka
 	io.println(v);
 	io.println(s + " " + t);
 	io.println(e);
-	for (int i = 0; i < e; ++i) {
-	    int a = 1, b = 2, c = 17;
-	    // Kant från a till b med kapacitet c
+
+    // Hantera source noden
+    for (int i = 1; i <= x; i++) {
+        int a = s, b = i, c = 1;
+        // Kant från a till b med kapacitet c
 	    io.println(a + " " + b + " " + c);
+    }
+	for (int i = 0; i < e; ++i) {
+	    currentedge = edges.get(i);
+        int a = currentedge.get(0), b = currentedge.get(1), c = 1;
+        io.println(a + " " + b + " " + c );
 	}
+
+    // Hantera sink noden 
+    for (int i = x+1; i <= y; i++) {
+        int a = i, b = t, c = 1;
+        io.println(a + " " + b + " " + c );
+    }
 	// Var noggrann med att flusha utdata när flödesgrafen skrivits ut!
 	io.flush();
 	
@@ -54,25 +77,33 @@ public class BipRed {
 	int t = io.getInt();
 	int totflow = io.getInt();
 	int e = io.getInt();
+    matchededges = new ArrayList<>();
 	
 	for (int i = 0; i < e; ++i) {
 	    // Flöde f från a till b
 	    int a = io.getInt();
 	    int b = io.getInt();
 	    int f = io.getInt();
+        if (f == 1) {
+            List<Integer> matchededge = new ArrayList<>();
+            matchededge.add(a);
+            matchededge.add(b);
+            matchededges.add(matchededge);
+        }
 	}
     }
     
     
     void writeBipMatchSolution() {
-	int x = 17, y = 4711, maxMatch = 0;
+	int x = x, y = y, maxMatch = matchededges.size();
 	
 	// Skriv ut antal hörn och storleken på matchningen
 	io.println(x + " " + y);
 	io.println(maxMatch);
 	
 	for (int i = 0; i < maxMatch; ++i) {
-	    int a = 5, b = 2323;
+        List<Integer> currentedge = matchededges.get(i);
+	    int a = currentedge.get(0), b = currentedge.get(1);
 	    // Kant mellan a och b ingår i vår matchningslösning
 	    io.println(a + " " + b);
 	}
@@ -101,4 +132,3 @@ public class BipRed {
 	new BipRed();
     }
 }
-
